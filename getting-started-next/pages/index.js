e96ff295f23fdccaf46daaed23c318be239a360a
Lucas Toledo <lucasmoraisss@gmail.com>
2022-02-02 09:06:16 -0300

    layout

--- getting-started-next/pages/index.js
+++ getting-started-next/pages/index.js
@@ -1,29 +1,35 @@
-import React from 'react';
-import Link from '../src/components/Link'
+import HomeScreen from "../src/screens/HomeScreen";
+
+export default HomeScreen;
+
+// import React from 'react';
+// import Link from '../src/components/Link'
+// import Footer from '../src/components/patterns/Footer';
 
 
 
-function Title({ children, as }) {
-  const Tag = as;
-  return (
-    <React.Fragment>
-      <Tag>{ children }</Tag>
-      <style jsx>{`
-        ${Tag} {
-          color: red;
-        }
-      `}</style>
-    </React.Fragment>
-  );
-}
+// function Title({ children, as }) {
+//   const Tag = as;
+//   return (
+//     <React.Fragment>
+//       <Tag>{ children }</Tag>
+//       <style jsx>{`
+//         ${Tag} {
+//           color: red;
+//         }
+//       `}</style>
+//     </React.Fragment>
+//   );
+// }
 
-export default function HomePage() {
-  return(
-    <div>
-      <Title as="h1">Alura Cases - Home Page</Title>
-      <Link  href="/faq"> 
-        Ir para a página de FAQ
-      </Link>
-    </div>
-  )
-}
+// export default function HomePage() {
+//   return(
+//     <div>
+//       <Title as="h1">Alura Cases - Home Page</Title>
+//       <Link  href="/faq"> 
+//         Ir para a página de FAQ
+//       </Link>
+//       <Footer />
+//     </div>
+//   )
+// }
